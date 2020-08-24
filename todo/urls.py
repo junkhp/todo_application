36c9046a74f3ce370e8dffdc577a2d264ec85bbd
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-08-24 16:37:35 +0900

    何もしない時のurlを追加

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('todo/', views.todo, name='todo'),
+    path('', views.TodoListView.as_view(), name='first'),
     path('list/', views.TodoListView.as_view(), name='list'),
     path('detail/<int:pk>', views.TodoDetailView.as_view(), name='detail'),
     path('create/', views.TodoCreateView.as_view(), name='create'),
